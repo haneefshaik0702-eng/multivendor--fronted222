b642a4167ad1099144762b6d6c4a895107dc8765
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-25 22:42:58 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // 👈 Make sure this line is here
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
