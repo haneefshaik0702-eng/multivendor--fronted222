ea67849d2d98a406ad92afdc764e482663d93fee
haneefshaik0702-eng <haneefshaik0702@gmail.com>
2025-10-26 00:26:08 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "https://ckend-6ozb.onrender.com";
+cconst BASE_URL = "https://multi-vendor-backend-6ozb.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
